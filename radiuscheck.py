c685d39e809afd9866f7e5e7142895115ae499dd
Brent Dreyer <bdreyer@ciena.com>
2018-08-24 12:27:05 +1000

    Main updated

--- radiuscheck.py
+++ radiuscheck.py
@@ -3,6 +3,7 @@ import os
 import configparser
 import sys
 
+#Loads configuration
 config = configparser.ConfigParser()
 
 config.read(options.ini)
@@ -12,16 +13,36 @@ rsecret =
 ruser
 rpass =
 
+#Checks the Radius ping resonse. Critical (2) if fail.
 def check_ping():
-    hostname =
-    response = os.system("ping -c 1 " + hostname)
+    response = os.system("ping -c 1 " + rip)
     if response == 0:
         pingstatus = "NOK"
     else:
         pingstatus = "NFAIL"
     return pingstatus
 
-if check_ping(pingstatus) = "NFAIL"
+#Checks the Radius configuration. Critical (2) if fail.
+def check_radius():
+    #If Null is confugured on port bypass#
+    return radiusstatus
+
+
+######
+#Main#
+######
+
+#If clear, OK (0). If Server details are left as Null, Warning (1).
+if check_ping(pingstatus) = "NFAIL":
         print "Unable to reach radius server IP"
-        sys.exit(1)
-    elif
+        sys.exit(2)
+    else:
+        if checkradius(radiusstatus) = "RCSUCCESS":
+                print "Radius Server can be reached and Auth Successful"
+                sys.exit(0)
+        elif checkradius(radiusstatus) = "NULL":
+                print "Radius server can be reached, but No Auth Configured"
+                sys.exit(1)
+        elif checkradius(radiusstatus) = "":
+                print "Radius server can be reached, but No Auth Configured"
+                sys.exit(1)
